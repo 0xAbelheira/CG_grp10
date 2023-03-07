c30e777f1175953c9f07611489bb2be0b27963d6
simaom21 <simaomatos.21@gmail.com>
2023-03-07 21:11:39 +0000

    esfera e cone

--- src/Generator/calcVertices.cpp
+++ src/Generator/calcVertices.cpp
@@ -157,6 +157,72 @@ figure generate::createBox(float size, float nrDiv){
     }
     return box;
 }
+
+// Função que cria o Esfera. Recebe o raio, slices e stacks
+figure generate::createSphere(float radius, int slices, int stacks){
+    float alpha = (float)2 * M_PI / slices;
+	float beta = (float)M_PI / stacks;
+
+    figure sphere;
+
+	for(int i = 0; i < slices; i++) {
+		//glVertex3f(0, radius, 0);
+		//glVertex3f(radius * sin(beta) * sin(alpha*i), radius * cos(beta), radius * sin(beta) * cos(alpha*i));
+		//glVertex3f(radius * sin(beta) * sin(alpha*(i+1)), radius * cos(beta), radius * sin(beta) * cos(alpha*(i+1)));
+
+		for(int j = 0; j < stacks; j++) {
+			sphere.addPoint(radius * sin(beta*j) * sin(alpha*i), radius * cos(beta*j), radius * sin(beta*j) * cos(alpha*i));
+			sphere.addPoint(radius * sin(beta*(j+1)) * sin(alpha*i), radius * cos(beta*(j+1)), radius * sin(beta*(j+1)) * cos(alpha*i));
+			sphere.addPoint(radius * sin(beta*j) * sin(alpha*(i+1)), radius * cos(beta*j), radius * sin(beta*j) * cos(alpha*(i+1)));
+
+			sphere.addPoint(radius * sin(beta*j) * sin(alpha*(i+1)), radius * cos(beta*j), radius * sin(beta*j) * cos(alpha*(i+1)));
+			sphere.addPoint(radius * sin(beta*(j+1)) * sin(alpha*i), radius * cos(beta*(j+1)), radius * sin(beta*(j+1)) * cos(alpha*i));
+			sphere.addPoint(radius * sin(beta*(j+1)) * sin(alpha*(i+1)), radius * cos(beta*(j+1)), radius * sin(beta*(j+1)) * cos(alpha*(i+1)));
+		}
+
+		//glVertex3f(radius * sin(beta) * sin(alpha*(i+1)), -radius * cos(beta), radius * sin(beta) * cos(alpha*(i+1)));
+		//glVertex3f(radius * sin(beta) * sin(alpha*i), -radius * cos(beta), radius * sin(beta) * cos(alpha*i));
+		//glVertex3f(0, -radius, 0);
+	}
+}
+
+// Função que cria o Cone. Recebe o raio da base, altura, slices e stacks
+figure generate::createCone(float radius, float height, int slices, int stacks){
+	float alpha = 2 * M_PI / slices;
+    float hTop, hLow;
+    float difH = height/stacks;
+    float difR = radius/stacks;
+    float rTop, rLow;
+
+    figure cone;
+
+	for(int i = 0; i < slices; i++) {
+        hLow = 0;
+        rLow = radius;
+        //base
+		cone.addPoint(radius*sin(alpha*(i+1)), 0, radius*cos(alpha*(i+1)));
+		cone.addPoint(radius*sin(alpha*i), 0, radius*cos(alpha*i));
+		cone.addPoint(0, 0, 0);
+
+        for(int j = 0; j < stacks; j++) {
+            rTop = rLow - difR;
+            hTop = hLow + difH;
+
+		    cone.addPoint(rLow*sin(alpha*i), hLow, rLow*cos(alpha*i));
+            cone.addPoint(rLow*sin(alpha*(i+1)), hLow, rLow*cos(alpha*(i+1)));
+		    cone.addPoint(rTop*sin(alpha*i), hTop, rTop*cos(alpha*i));
+
+            cone.addPoint(rLow*sin(alpha*(i+1)), hLow, rLow*cos(alpha*(i+1)));
+            cone.addPoint(rTop*sin(alpha*(i+1)), hTop, rTop*cos(alpha*(i+1)));
+            cone.addPoint(rTop*sin(alpha*i), hTop, rTop*cos(alpha*i));
+            rLow = rTop;
+            hLow += difH;
+        }
+	}
+
+    return cone;
+}
+
 /* ESPERAR PELA AULA PRÁTICA
 
 // Função que cria a esfera 
